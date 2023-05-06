30d8ae48c3379db810b03cd93a41f85b845d8545
sujit-jagdale12 <sujitjagdale12@gmail.com>
2023-05-06 21:21:53 +0530

    safe commit : user can able to book event on ticket type

--- dashboard/userscreen/view-user.js
+++ dashboard/userscreen/view-user.js
@@ -43,9 +43,35 @@ function showSuccessModal() {
     const modal = new bootstrap.Modal(myModalEl)
     modal.show()
 }
+
 apiGetEventDetails()
 
 function logOut() {
     localStorage.setItem("userId", null)
     window.location.href = "../../loginpage/login.html"
+}
+
+function showSuccessModalEventBook() {
+    const myModalEl = document.getElementById('successModalEventByUserId');
+    const modal = new bootstrap.Modal(myModalEl)
+    modal.show()
+}
+function bookEventByUserId() {
+    const userId = localStorage.getItem("userId");
+    const eventId = readIdQueryParam()
+
+    const headers = {
+        'content-type': 'application/json'
+    }
+    axios.post(`http://localhost:8080/attendee/${userId}/event/${eventId}`, { headers })
+
+        .then(res => {
+            showSuccessModalEventBook()
+            hideSetBookEvent()
+        }).catch(err => console.log(err))
+}
+
+function hideSetBookEvent() {
+    const container = document.getElementById("successModal");
+    container.style.display = "none";
 }
