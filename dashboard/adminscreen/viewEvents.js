69d44391713b3b572ae1a0ab66a19e2d04583087
sujit-jagdale12 <sujitjagdale12@gmail.com>
2023-05-25 10:23:13 +0530

    view new events  first

--- dashboard/adminscreen/viewEvents.js
+++ dashboard/adminscreen/viewEvents.js
@@ -4,29 +4,25 @@ function setupTable() {
 }
 
 setupTable()
-
 function propulateActualData(table, events) {
+    for (let i = events.length - 1; i >= 0; i--) {
+        const event = events[i];
+        const { id, title, startdate, enddate, location, time } = event;
+        const updatePageUrl = `./update-event.html?id=${id}`;
 
-    for (const event of events) {
-
-        const { id, title, startdate, enddate, location, time } = event
-        const updatePageUrl = `./update-event.html?id=${id}`
-
-        const row = table.insertRow()
-        row.insertCell(0).innerHTML = id
-        row.insertCell(1).innerHTML = title
-        row.insertCell(2).innerHTML = startdate
-        row.insertCell(3).innerHTML = enddate
-        row.insertCell(4).innerHTML = location
-        row.insertCell(5).innerHTML = time
-        row.insertCell(6).innerHTML = `
-    <a class='ms-5' style="font-size:24px;color:blue" href='${updatePageUrl}'><i class='fas fa-eye'></i></a>
-    <a class='ms-5' style="font-size:24px;color:red" onclick='showConfirmDeleteModal(${id})'><i class='fas fa-trash-alt'></i></a>
-`;
-
-
+        const row = table.insertRow();
+        row.insertCell(0).innerHTML = title;
+        row.insertCell(1).innerHTML = startdate;
+        row.insertCell(2).innerHTML = enddate;
+        row.insertCell(3).innerHTML = location;
+        row.insertCell(4).innerHTML = time;
+        row.insertCell(5).innerHTML = `
+            <a class='ms-5' style="font-size:24px;color:blue" href='${updatePageUrl}'><i class='fas fa-eye'></i></a>
+            <a class='ms-5' style="font-size:24px;color:red" onclick='showConfirmDeleteModal(${id})'><i class='fas fa-trash-alt'></i></a>
+        `;
     }
 }
+
 
 function showConfirmDeleteModal(id) {
     console.log('clicked ' + id)
